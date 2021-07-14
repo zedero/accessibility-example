891408d8748b919374a3a3b2cd7eee1fa9c0a560
Patrick Kersten <info@zedero.nl>
2021-07-14 09:25:47 +0200

    rounded border

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss',],
+  styleUrls: ['./button.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ButtonComponent implements OnInit {
